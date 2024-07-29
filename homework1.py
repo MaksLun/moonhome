14b21115106e71d3e2ca4db82e45d90f6915c569
MaksLun <m.lunev2014@yandex.ru>
2024-07-29 16:29:13 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and setti# See PyCharm help at https://www.jetbrains.com/help/pyc
+example = "tapinambr"
+print (example[0])
+print (example[-1])
+print (example[4:9])
+print (example[::-1])
+print (example[1:9:])
+
+
