31eb5220f35818980f5485de2dd63203422fc23f
MaksLun <m.lunev2014@yandex.ru>
2024-07-30 19:37:19 +0300

    Add files via upload
    мodul 1_3, динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = "Maksim"
+print(name, type(name))
+age = 24
+print(age, type(age))
+new_age = 25
+print(new_age, type(new_age))
+it_student = '25'
+print(it_student, type(it_student))
+print ('name',name)
+print ('age',age)
+print ('new age',new_age)
+print ('it student', bool(it_student))
