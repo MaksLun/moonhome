30770bcfdbf384e83d0688c3fec97bfb8cab2e11
MaksLun <m.lunev2014@yandex.ru>
2024-07-30 17:59:38 +0300

    Add files via upload
    модуль 1, переменные

---
+++ module_1_2.py
@@ -0,0 +1,9 @@
+number_of_tasks = 12
+hours_spent = 1.5
+name_of_the_course = 'Python'
+time_for_one_task = (hours_spent / number_of_tasks)
+print ('курс:',name_of_the_course,',','всего задач:',number_of_tasks,',',
+       'затрачено часов:', hours_spent,',', 'среднее время выполнения:', time_for_one_task,'часа.')
+
+
+
