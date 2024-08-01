1db34b2ffd519bf3dd3e80aca7fd005d1ef4b8e9
MaksLun <m.lunev2014@yandex.ru>
2024-08-01 23:39:58 +0300

    Add files via upload
    module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+#первая часть
+my_string = input("введите слово: ")
+quantity_symbol = len(my_string)
+print("количество символов: ",quantity_symbol)
+#вторая
+print(my_string.upper())
+print(my_string.lower())
+print(my_string .replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
