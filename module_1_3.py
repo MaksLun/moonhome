01b50d630108ef06214eaf5d4f1e10ffe29c877e
MaksLun <m.lunev2014@yandex.ru>
2024-08-01 21:46:17 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,11 +2,10 @@ name = "Maksim"
 print(name, type(name))
 age = 24
 print(age, type(age))
-new_age = 25
-print(new_age, type(new_age))
-it_student = '25'
-print(it_student, type(it_student))
-print ('name',name)
-print ('age',age)
-print ('new age',new_age)
-print ('it student', bool(it_student))
+age = age + 1
+print(age, type(age))
+print('name',name)
+print('age',age - 1)
+print('new age',age)
+is_student = '25'
+print('is student', bool(is_student))
