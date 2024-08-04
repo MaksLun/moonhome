196c1d48c03c778b11837369ab7040f0af46f6d4
MaksLun <m.lunev2014@yandex.ru>
2024-08-04 19:37:58 +0300

    modul1.6

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+#словари
+my_dict = {'Maksim': 1999, 'Dima': 1998, 'Lexus': 1999}
+print(my_dict)
+print(my_dict['Maksim'])
+my_dict['Artem'] = 1999
+print(my_dict['Artem'])
+my_dict.update({'Masha': 1995, 'Alina':2001})
+print(my_dict)
+a = my_dict.pop('Masha')
+print(a)
+print(my_dict)
+#множества
+my_set = {1, 2, 2, 3, 4, 1, 2, 3, 'max', (1, 2, 3)}
+print(my_set)
+print(my_set.add(7))
+print(my_set.add('new'))
+print(my_set)
+my_set.remove('max')
+print(my_set)
+
