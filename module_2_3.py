05134b3f2c0a26119cc68b9b33e3937de976e79d
MaksLun <m.lunev2014@yandex.ru>
2024-08-16 11:49:59 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,3 @@
-#while 1 > 0:
-#    number = int(input("Введите число: "))
-#    if number % 2 == 0:
-#        print("четное число")
-#        continue
-#    else:
-#        print("нечетное число")
-#    print("меня не забыли")
-#print("я здесь")
 my_string = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_string):
